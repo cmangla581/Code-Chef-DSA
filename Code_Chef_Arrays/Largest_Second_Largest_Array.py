7060a25a77e4773a21365c40390aeec170f1ca93
Chaitanya Mangla <tafscm@gmail.com>
2026-08-17 16:24:11 +0530

    Add files via upload

---
+++ Code_Chef_Arrays/Largest_Second_Largest_Array.py
@@ -0,0 +1,15 @@
+
+t = int(input()) 
+
+for _ in range(t): 
+    n = int(input()) 
+    a = list(map(int, input().split()))
+
+    m1, m2 = max(a), 0 
+
+    for x in a : 
+        if x != m1:m2 = max(m2, x)
+
+    print(m1 + m2) 
+
+    t -= 1 
